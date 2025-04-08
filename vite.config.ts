a270a3805437eaf836aa6d64c094d4bc94f1628d
egkike <egkike@gmail.com>
2025-04-08 15:46:04 -0300

    Se modifica index.html y vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Landing-Rodri/' : '/',
-})
+  base: process.env.NODE_ENV === "production" ? "/Landing-Rodri/" : "/",
+  resolve: {
+    alias: {
+      "/main.tsx": path.resolve(__dirname, "src/main.tsx"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
