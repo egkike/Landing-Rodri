46d7e117700282ddc957fa01c7d9a08d6c44ea02
egkike <egkike@gmail.com>
2025-04-10 21:01:34 -0300

    Cambios en vite.config.js, index.html y main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -8,9 +7,7 @@ const basename =
   process.env.NODE_ENV === "production" ? "/Landing-Rodri/" : "/";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/Landing-Rodri/" : "/",
-  resolve: {
-    alias: {
-      "/main.tsx": path.resolve(__dirname, "src/main.tsx"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    // Asegúrate de que Vite use el index.html como entrada
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-    },
-  },
 });
